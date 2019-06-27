0aa533e05c0c20f8f5c81d5f516360287e09dadb
jaydonhashimoto <jaydonhashi@gmail.com>
2019-06-27 16:14:33 -0700

    added user model and api route

---
+++ routes/api/users.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model
+const User = require('../../models/User');
+
+/**
+ * @route POST api/users
+ * @desc Register new user
+ * @access Public
+ */
+router.post('/', (req, res) => {
+    const { name, email, password } = req.body;
+
+    //simple validation
+    if (!name || !email || !password) {
+        return res.status(400).json({ msg: 'Please enter all fields' });
+    }
+
+    //check for existing user
+    User.findOne({ email })
+        .then(user => {
+            //check user is null or not
+            if (user) {
+                return res.status(400).json({ msg: 'User already exists' });
+            }
+
+            const newUser = new User({
+                name,
+                email,
+                password
+            });
+
+            //create salt and hash
+            //salt is used to create hash from plaintext
+            //genSalt takes in rounds (the higher the better, but takes longer)
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+
+                    //set pass to new hash
+                    newUser.password = hash;
+
+                    //send data to db and send user res
+                    newUser.save()
+                        .then(user => {
+                            res.json({
+                                user: {
+                                    id: user.id,
+                                    name: user.name,
+                                    email: user.email
+                                }
+                            });
+                        });
+                })
+            })
+        })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
-
 
 const app = express();
 
 //bodyParser middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 //db config
 const db = require('./config/keys').mongoURI;
 
 //connect to Mongo
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useCreateIndex: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 //use routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
